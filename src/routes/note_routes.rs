ec677237ffc8ace7913e613840c648d6d85ff0a8
Divyansh <divyansh.shrivastava@students.iiit.ac.in>
2023-04-18 15:59:32 +0530

    Pickup parameters from Path

--- src/routes/note_routes.rs
+++ src/routes/note_routes.rs
@@ -1,13 +1,17 @@
-pub async fn get_note_with_id() -> &'static str {
-    "Hello from get_note_with_id"
+use axum::extract::Path;
+
+pub async fn get_note_with_id(Path(id): Path<String>) -> String {
+    format!("Hello from get note with id: {}", id)
 }
 
-pub async fn delete_note_with_id() -> &'static str {
-    "Hello from delete note with id"
+pub async fn delete_note_with_id(Path(id): Path<String>) -> String {
+    format!("Hello from delete note with id: {}", id)
 }
+
 pub async fn get_all_notes() -> &'static str {
     "Hello from get all notes"
 }
+
 pub async fn add_note() -> &'static str {
     "Hello from add note!"
 }
