de72b466914e15e9a4a3ff54053e72b4352c412e
Divyansh <divyansh.shrivastava@students.iiit.ac.in>
2023-04-22 13:48:42 +0530

    Add Cors Layer middleware

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -3,15 +3,21 @@ mod root_route;
 mod user_routes;
 
 use axum::{
+    http::Method,
     routing::{get, post},
     Router,
 };
+use tower_http::cors::{Any, CorsLayer};
 
 use note_routes::*;
 use root_route::root_route;
 use user_routes::*;
 
 pub async fn get_router() -> Router {
+    let cors_layer = CorsLayer::new()
+        .allow_methods([Method::GET, Method::POST, Method::DELETE])
+        .allow_origin(Any);
+
     Router::new()
         .route("/", get(root_route))
         .route("/signup", post(handle_signup))
@@ -22,4 +28,5 @@ pub async fn get_router() -> Router {
         )
         .route("/notes", get(get_all_notes))
         .route("/notes", post(add_note))
+        .layer(cors_layer)
 }
