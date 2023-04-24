3ffc5745ddb9221d781ba65cce937403f7adc726
Divyansh <divyansh.shrivastava@students.iiit.ac.in>
2023-04-24 12:54:20 +0530

    Added Verbosity to curl commands

--- Scripts/test.sh
+++ Scripts/test.sh
@@ -1,28 +1,34 @@
 #!/bin/bash
 
-echo -e "Testing POST /signup"
-curl --header "Content-Type: application/json" \
-	--request POST \
-	--data '{"username":"Rust_is_OP", "password": "CrabLangLuL"}' \
-	http://localhost:3000/signup
+# echo -e "Testing POST /signup"
+# curl --verbose --header "Content-Type: application/json" \
+# 	--request POST \
+# 	--data '{"username":"Rust_is_OP", "password": "CrabLangLuL"}' \
+# 	http://localhost:3000/signup
+echo -e "\n-----------------------------------------------"
 
 echo -e "\n\nTesting POST /login"
-curl --header "Content-Type: application/json" \
+curl --verbose --header "Content-Type: application/json" \
 	--request POST \
 	--data '{"username":"test_username", "password": "pass123"}' \
 	http://localhost:3000/login
+echo -e "\n-----------------------------------------------"
 
 echo -e "\n\nTesting GET /notes/:id"
-curl http://localhost:3000/notes/123
+curl --verbose http://localhost:3000/notes/123
+echo -e j"\n-----------------------------------------------"
 
 echo -e "\n\nTesting GET /notes"
-curl http://localhost:3000/notes
+curl --verbose http://localhost:3000/notes
+echo -e "\n-----------------------------------------------"
 
 echo -e "\n\nTesting POST /notes"
-curl --header "Content-Type: application/json" \
+curl --verbose --header "Content-Type: application/json" \
 	--request POST \
 	--data '{"title":"test_title", "note": "note body"}' \
 	http://localhost:3000/notes
+echo -e "\n-----------------------------------------------"
 
 echo -e "\n\nTesting DELETE /notes/:id"
-curl --request DELETE http://localhost:3000/notes/123
+curl --verbose --request DELETE http://localhost:3000/notes/123
+echo -e "\n-----------------------------------------------"
